19e032c2154ad3fcf9af9bac6ca0356285319e43
micheal <michealtesfay@gmail.com>
2018-05-06 20:39:49 +0100

    creating Airbnb clone using react

---
+++ airbnbclone/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+//import Hello from './components/Hello.js'
+
+ReactDOM.render(<App />, document.getElementById('root'));
